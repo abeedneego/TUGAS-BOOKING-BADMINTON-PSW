cf7ed8863132b5d6da9605167c10ecebe298179a
abeedneego <hiz.abednego@gmail.com>
2024-06-07 11:31:48 +0700

    uas_prak_002

---
+++ resources/views/components/application-mark.blade.php
@@ -0,0 +1,4 @@
+<div class="inline-flex items-center">
+  <img src="{{ asset('images/badminku.jpg') }}" alt="" class="h-16 w-16">
+  <h1 class="text-3xl font-bold text-pink-500"> Balige Sport </h1>
+</div>
